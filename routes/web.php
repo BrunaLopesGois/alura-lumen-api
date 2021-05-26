5392ce60c073180921b023d4f06a79345f16d1f2
BrunaLopesGois <bruna-chan@live.com>
2021-05-26 13:44:33 -0300

    manipulating api resources

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,9 @@ $router->get('/', function () use ($router) {
 });
 
 $router->group(['prefix' => '/api'], function () use ($router) {
+    $router->post('/series', 'SeriesController@store');
     $router->get('/series', 'SeriesController@index');
+    $router->get('/series/{id}', 'SeriesController@show');
+    $router->put('/series/{id}', 'SeriesController@update');
+    $router->delete('/series/{id}', 'SeriesController@destroy');
 });
